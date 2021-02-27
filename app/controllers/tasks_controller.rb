02553b2ef6512130e54da87b0622fdf246dddadb
ryuto.inose <dragon101114levi@yahoo.co.jp>
2021-02-27 11:25:07 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,17 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
  
   def index
     if logged_in?
+      @task = current_user.tasks.build 
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -29,12 +31,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end  
 
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to root_url
@@ -45,13 +44,16 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end  
   
   def task_params
     params.require(:task).permit(:content, :status)
